13d0e168acb0fd394f14aa15c7703b29d878b1cc
shrey <shrey.nautiyal07@gmail.com>
2024-09-21 11:52:00 +0400

    Added the glasses and the background

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Sidebar = () => {
   return (
     <div>
-      <img src = {"Logo.png"} width = "100" height = "80"></img>
+
     </div>
   );
 };
--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,19 +1,18 @@
 import React from "react";
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Heading, Image } from "@chakra-ui/react";
 
 const Welcome_Page = () => {
   return (
-      <Heading
-        as="h1"           // Use as <h1>
-        size="4xl"         // Font size (extra-large)
-        color="black.500"  // Text color (teal)f
-        display = "flex"
-        margin = "180px auto"
-        marginLeft= "200px"
-      >
-        Welcome to
-      </Heading>
-    
+    <Box
+      backgroundImage="url('background.png')"
+      backgroundSize="cover"
+      backgroundPosition="center"
+      height="100vh"
+    >
+      <Box justifyContent= "center" display = "flex" alignItems= "center" height = "100vh">
+        <Image src="glasses.png" alt="Glasses Image" h = "400px" w = '400px' />
+      </Box>
+    </Box>
   );
 };
 
