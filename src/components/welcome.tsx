2dba156c321cd223024f21573ed4b492b00739ea
Hatim <106645329+Hatima4@users.noreply.github.com>
2024-09-21 13:18:44 +0400

    fixed

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Box, Heading, Image } from "@chakra-ui/react";
-import { Box, Heading, Image } from "@chakra-ui/react";
 
 const Welcome_Page = () => {
   return (
