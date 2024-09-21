a199986876ffdb9535c014586aedc6ad15144f60
shrey <shrey.nautiyal07@gmail.com>
2024-09-22 00:21:36 +0400

    added specs not glasses and rearranged welcome.tsx

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -1,48 +1,50 @@
-import React from "react";
-import { Box, Heading, Image, Text, Button } from "@chakra-ui/react";
+import { Box, Text, Button, Image, Flex } from '@chakra-ui/react';
 
 const Welcome_Page = () => {
   return (
     <Box
-      backgroundSize="cover"
-      backgroundPosition="center"
       height="100vh"
-      position="relative"
+      background="linear-gradient(to bottom, #ffffff, #f0f0f0)" // Subtle gradient background
+      textAlign="center"
+      display="flex"
+      flexDirection="column"
     >
-      {/* Glasses image section */}
-      <Box
-        justifyContent="center"
-        display="flex"
-        alignItems="center"
-        height="80vh"
-      >
-        <Image src="glasses.png" alt="Glasses Image" h="400px" w="400px" />
+      {/* Flex container for Welcome Text and Glasses */}
+      <Flex justifyContent="space-between" alignItems="center" height="80vh">
         
-        {/* Welcome text, positioned over the image */}
-        <Text
-          fontSize="4xl"
-          fontWeight="bold"
-          color="white"
-          position="absolute"
-          bottom="190px"  // Adjust the position for better fit
-          textShadow="0px 0px 10px rgba(0, 0, 0, 0.8)"
-        >
-          Welcome to our vision
-        </Text>
-      </Box>
+        {/* Welcome Text and Button */}
+        <Box flex="1" paddingLeft="100px">  {/* Swapped to the left */}
+          <Text fontSize="5xl" fontWeight="bold">
+            Welcome to our vision
+          </Text>
 
-      
-      <Box position = "absolute" bottom="60px" width="100%" textAlign="center">
-        <Button 
-          color="white"  // Change text color to white
-          borderColor="white"  // Add white border for the outline variant
-          variant="outline"
-          fontWeight="bold"
-          fontSize="24px"
-        >
-          Scroll Down
-        </Button>
-      </Box>
+          <Text fontSize="lg" color="gray.500" fontStyle="italic" mt={2}>
+            Because all vision matters
+          </Text>
+
+          <Button
+            mt={6}
+            colorScheme="teal"
+            size="lg"
+            fontWeight="bold"
+            borderRadius="lg"
+            _hover={{ boxShadow: '0 0 10px rgba(0, 212, 255, 0.8)' }}
+          >
+            Learn More
+          </Button>
+        </Box>
+
+        {/* Glasses Image with individual positioning */}
+        <Box flex="1" paddingRight="100px"> {/* Swapped to the right */}
+          <Image
+            src="specs.png"
+            alt="Glasses Image"
+            h="500px"
+            w="500px"
+          />
+        </Box>
+        
+      </Flex>
     </Box>
   );
 };
