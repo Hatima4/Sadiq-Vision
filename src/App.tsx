4773fa8c4d6823e0c295b0905a4b432b35d7f68f
shrey <shrey.nautiyal07@gmail.com>
2024-09-23 23:07:08 +0400

    centered team added my pfp(sigma black) added emailing feature literally took like 10 mins lol

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar.tsx";
 import Welcome_Page from "./components/welcome.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import Aboutpage from "./components/About.tsx";
-import Team from "./components/team.tsx";
+import Team from "./components/team.tsx"
 import Contactus from "./components/Contactus.tsx";
 
 function App() {
--- src/components/team.tsx
+++ src/components/team.tsx
@@ -19,9 +19,15 @@ const Team = () => {
       </Box>
 
       {/* Team Members */}
-      <HStack paddingTop="20px" spacing={8} justify="center">
+      <HStack
+        paddingTop="20px"
+        justify="space-between"
+        alignItems="center"
+        width="80%"
+        mx="auto"
+      >
         {/* Qasim */}
-        <VStack align="center">
+        <VStack align="center" textAlign="center" mr = {5} width = "60%" paddingTop={8}>
           <Circle size="205px" border="3px solid black">
             <Link href="https://www.instagram.com/whiz_qasim786/">
               <Image
@@ -29,25 +35,30 @@ const Team = () => {
                 objectFit="cover"
                 boxSize="100%"
                 borderRadius="100px"
-              ></Image>
+              />
             </Link>
           </Circle>
           <Text color="black" fontWeight="bold" fontSize="3xl">
             Qasim
           </Text>
-          Image
           <Text color="grey" fontWeight="bold" fontSize="xl">
             NGO Owner
           </Text>
           <Text color="grey" fontSize="sm" textAlign="center">
-            
-An IB Diploma student, has always had a passion to make a tangible difference. Qasim leads initiatives that collect and distribute eyeglass frames to those in need, particularly in Pakistan. His innovative approach and commitment to sustainable and long-term solutions for the impoverished drives the organization's mission to ensure that vision care is accessible to all.
+            I'm an IB Diploma student passionate about making a difference. I lead initiatives to collect and distribute eyeglass frames to those in need, especially in Pakistan. My focus is on creating sustainable, long-term solutions to help the less fortunate get access to vision care.
           </Text>
         </VStack>
 
         {/* Shrey */}
-        <VStack align="center">
-          <Circle size="200px" border="3px solid black" />
+        <VStack align="center" textAlign="center" width = "60%">
+          <Circle size="200px" border="3px solid black">
+            <Image
+              src="black.jpeg"
+              objectFit="cover"
+              boxSize="100%"
+              borderRadius="100px"
+            />
+          </Circle>
           <Text color="black" fontWeight="bold" fontSize="3xl">
             Shrey
           </Text>
@@ -55,14 +66,12 @@ An IB Diploma student, has always had a passion to make a tangible difference. Q
             Web Developer
           </Text>
           <Text color="grey" fontSize="sm" textAlign="center">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-            lacinia odio vitae vestibulum
-            vestibulumdadjaisdjasioudjasodashjoudas
+            JESS IB Student | Interested in math and programming | Can write in Python, Typescript and web development languages | Aspiring freelance web developer
           </Text>
         </VStack>
 
         {/* Hatim */}
-        <VStack align="center">
+        <VStack align="center" textAlign="center" width = "60%">
           <Circle size="205px" border="3px solid black">
             <Link href="https://www.instagram.com/bigbadh4/">
               <Image
@@ -72,13 +81,12 @@ An IB Diploma student, has always had a passion to make a tangible difference. Q
                 borderRadius="200px"
                 height="200px"
                 width="200px"
-              ></Image>
+              />
             </Link>
           </Circle>
           <Text color="black" fontWeight="bold" fontSize="3xl">
             Hatim
           </Text>
-
           <Text color="grey" fontWeight="bold" fontSize="xl">
             Web Developer
           </Text>
